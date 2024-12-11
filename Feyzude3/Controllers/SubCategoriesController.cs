fdf4ae88531b2ec506d8ecb5f2a1b8b39d2b5291
Feyza <ezberfeyza@gmail.com>
2024-12-11 20:03:41 +0300

    admin paneli oluşturuldu

---
+++ Feyzude3/Controllers/SubCategoriesController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Feyzude3.Data;
+using Feyzude3.Models;
+
+namespace Feyzude3.Controllers
+{
+    public class SubCategoriesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SubCategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: SubCategories
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.SubCategories.ToListAsync());
+        }
+
+        // GET: SubCategories/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var subCategory = await _context.SubCategories
+                .FirstOrDefaultAsync(m => m.SubCategory_Id == id);
+            if (subCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(subCategory);
+        }
+
+        // GET: SubCategories/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SubCategories/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("SubCategory_Id,SubCategory_Name,Category_Id")] SubCategory subCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(subCategory);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(subCategory);
+        }
+
+        // GET: SubCategories/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var subCategory = await _context.SubCategories.FindAsync(id);
+            if (subCategory == null)
+            {
+                return NotFound();
+            }
+            return View(subCategory);
+        }
+
+        // POST: SubCategories/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("SubCategory_Id,SubCategory_Name,Category_Id")] SubCategory subCategory)
+        {
+            if (id != subCategory.SubCategory_Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(subCategory);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SubCategoryExists(subCategory.SubCategory_Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(subCategory);
+        }
+
+        // GET: SubCategories/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var subCategory = await _context.SubCategories
+                .FirstOrDefaultAsync(m => m.SubCategory_Id == id);
+            if (subCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(subCategory);
+        }
+
+        // POST: SubCategories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var subCategory = await _context.SubCategories.FindAsync(id);
+            if (subCategory != null)
+            {
+                _context.SubCategories.Remove(subCategory);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SubCategoryExists(int id)
+        {
+            return _context.SubCategories.Any(e => e.SubCategory_Id == id);
+        }
+    }
+}
